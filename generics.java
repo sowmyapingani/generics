7019dd7dbc905d572c9b826c2e275a8325a8979c
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-07-10 13:43:53 +0530

    added:value

--- generics.java
+++ generics.java
@@ -32,7 +32,19 @@ public class generics {    public Integer testMaxium(Integer x, Integer y, Integ
         max = z;
     }
     return max;
+}public class GenericClass<T extends Comparable <T>> {
+    public void testMaxium(T... n) {
+        Integer i=0;
+        T max = n[i];
+        for (T a:n) {
+            if (n[i].compareTo(max) > 0)
+                max = n[i];
+            i++;
+        }
+        System.out.println("The maximum Value is : "+max);
+    }
 }
+
 }
 
 
--- genericsmain.java
+++ genericsmain.java
@@ -10,7 +10,13 @@ public class genericsmain {
             System.out.println("The Maximum Integer is : "+genericClass.testMaxium(1,7,4));
             System.out.println("The Maximum Float is : "+genericClass.testMaxium(1.3f,4.5f,7.7f));
             System.out.println("The Maximum String is : "+genericClass.testMaxium("apple","banana","peach"));
-        }
+        } int in1 =4, in2 =6 , in3 =1, in4 =1;
+        String s1="deer", s2="rino", s3="lion", s4 ="bear";
+        GenericClass genericClass =new GenericClass();
+        genericClass.testMaxium(in1,in2,in3,in4);
+        genericClass.testMaxium(s1,s2,s3);
+    }
+}
 
         }
     }
